c3997d502feeb6dee0799fd66694a1f9dcac6840
benjamin32561 <ben32561@gmail.com>
2022-12-13 09:56:29 +0200

    fixed srevr stuff

--- Server Usage/AccidentClassifier.cs
+++ Server Usage/AccidentClassifier.cs
@@ -12,7 +12,6 @@ namespace Server_Usage
     class AccidentClassifier
     {
         private InferenceSession session;
-        private DenseTensor<float> input_template = new DenseTensor<float>(new[] { 1, 677, 3 });
         private float threshold;
 
         public AccidentClassifier(string model_path, float threshold = 0.5f)
@@ -21,19 +20,33 @@ namespace Server_Usage
             this.threshold = threshold;
         }
 
-        public float MakePrediction(float[,,] recording)
+        private DenseTensor<float> ArrayToDenseTensor(float[,] recording)
         {
-            if (recording.GetLength(0) != 1 || recording.GetLength(1) != 677 || recording.GetLength(2) != 3)
-                throw new ArgumentException("recording should be float[1,677,3]");
-            var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("modelInput", this.input_template) };
+            DenseTensor<float> to_ret = new DenseTensor<float>(new[] { 1, 677, 3 });
+            for(int i = 0; i<recording.GetLength(0);i++)
+            {
+                for (int j = 0; j < recording.GetLength(1); j++)
+                {
+                    to_ret[0,i,j] = recording[i,j];
+                }
+            }
+            return to_ret;
+        }
+
+        public float MakePrediction(float[,] recording)
+        {
+            if (recording.GetLength(0) != 677 || recording.GetLength(1) != 3)
+                throw new ArgumentException("recording should be float[677,3]");
+            DenseTensor<float> arr_as_tens = this.ArrayToDenseTensor(recording);
+            var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("modelInput", arr_as_tens) };
             var output = this.session.Run(inputs).First().AsTensor<float>();
             return output[0];
         }
 
-        public bool AccidentOrNot(float[,,] recording)
+        public bool AccidentOrNot(float[,] recording)
         {
-            if (recording.GetLength(0) != 1 || recording.GetLength(1) != 677 || recording.GetLength(2) != 3)
-                throw new ArgumentException("recording should be float[1,677,3]");
+            if (recording.GetLength(0) != 677 || recording.GetLength(1) != 3)
+                throw new ArgumentException("recording should be float[677,3]");
             float prediction = this.MakePrediction(recording);
             return prediction >= this.threshold;
         }
