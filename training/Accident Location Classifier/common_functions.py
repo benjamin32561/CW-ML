d61a159c9083fc881c4d3ec9d1fe3c7779b1d9d7
benjamin32561 <ben32561@gmail.com>
2022-12-14 13:15:47 +0200

    fixxed

--- training/Accident Location Classifier/common_functions.py
+++ training/Accident Location Classifier/common_functions.py
@@ -8,9 +8,12 @@ from torch.nn.functional import one_hot
 from data_analysis.functions import GetDataFrame, PlotRecordData, CreatePath
 
 class AccidentLocationClassifierAcceleratorDataset(Dataset):
-    def __init__(self, file_list):
-        self.files = open(file_list,'r').read().split('\n')
+    def __init__(self, file_list, num_class):
+        f = open(file_list)
+        self.files = f.read().split('\n')
+        f.close()
         self.n_files = len(self.files)
+        self.nc = num_class
 
     def __len__(self):
         return self.n_files
@@ -18,7 +21,7 @@ class AccidentLocationClassifierAcceleratorDataset(Dataset):
     def __getitem__(self, idx, as_df=False):
         path, record_class = self.files[idx].split('\t')
         class_to_ret = np.array([int(record_class)])
-        one_hot_enc_to_ret = one_hot(tensor(class_to_ret)).to(float32)
+        one_hot_enc_to_ret = one_hot(tensor(class_to_ret),num_classes=self.nc).to(float32)
         df = GetDataFrame(path)
         if as_df:
             return df,one_hot_enc_to_ret
