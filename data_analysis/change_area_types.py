4ec2d056b90705ad13f33b8cba7c23bf1dc0c25d
benjamin32561 <ben32561@gmail.com>
2022-12-14 12:32:58 +0200

    renamed some stuff, started working on accident location classifier

---
+++ data_analysis/change_area_types.py
@@ -0,0 +1,39 @@
+import argparse
+import glob
+from os import rename
+from os.path import join, basename, exists
+from functions import GetDataFrame, CalcFrequency, CreatePath
+
+#python change_area_types.py --org_type_txt_path "" --new_type 1
+
+AREA_DIVISION = {1:{'1':'1', '2':'1', '3':'2', '4':'3', '5':'3', '6':'4', '7':'5', '8':'5', '9':'6', '10':'7', '11':'7', '12':'8',},
+                2:{'1':'1', '2':'0', '3':'0', '4':'0', '5':'0', '6':'0', '7':'0', '8':'0', '9':'0', '10':'0', '11':'0', '12':'0',}}
+
+def main(args=None):
+    parser = argparse.ArgumentParser(description='Simple script for changing annotation class.')
+
+    parser.add_argument('--org_type_txt_path', help='Path to .txt file containing .xlsx files path and their class')
+    parser.add_argument('--new_type', help='int representing the new area division', type=int)
+    parser = parser.parse_args(args)
+
+    assert parser.org_type_txt_path!=None, "org_type_txt_path can't be None"
+    assert exists(parser.org_type_txt_path), "org_type_txt_path does not exist"
+
+    new_division = AREA_DIVISION[parser.new_type]
+    
+    f = open(org_type_txt_path)
+    data = f.read()
+    f.close()
+
+    lines = data.split('\n')
+    new_lines = []
+    for line in lines:
+        f_path, class_type = line.split('\t')
+        new_class_type = new_division[class_type]
+        new_lines.append(f_path+'\t'+new_class_type)
+    f = open(org_type_txt_path,'w')
+    f.write('\n'.join(new_lines))
+    f.close()
+        
+if "__main__"==__name__:
+    main()
---
+++ training/Accident Location Classifier/common_functions.py
@@ -0,0 +1,28 @@
+import sys
+import os
+sys.path.append(os.path.abspath('/content/Cloud-Wise-ML/'))
+from torch.utils.data import Dataset
+from data_analysis.functions import GetDataFrame, PlotRecordData, CreatePath
+
+import sys
+import os
+sys.path.append(os.path.abspath('/content/Cloud-Wise-ML/'))
+import numpy as np
+from torch.utils.data import Dataset
+from data_analysis.functions import GetDataFrame, PlotRecordData, CreatePath
+
+class ClassifierAcceleratorDataset(Dataset):
+    def __init__(self, file_list):
+        self.files = open(file_list,'r').read().split('\n')
+        self.n_files = len(self.files)
+
+    def __len__(self):
+        return self.n_files
+
+    def __getitem__(self, idx, as_df=False):
+        path, record_class = self.files[idx].split('\t')
+        class_to_ret = np.array([float(record_class)])
+        df = GetDataFrame(path)
+        if as_df:
+            return df,class_to_ret
+        return df.to_numpy()[:,:-1],class_to_ret
