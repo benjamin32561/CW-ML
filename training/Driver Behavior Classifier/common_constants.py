ea502a8b1c9cbb63e69d8cc300daa85533ea4975
benjamin32561 <ben32561@gmail.com>
2023-01-02 17:04:46 +0200

    added code for driver behavior classifier

---
+++ training/Driver Behavior Classifier/common_constants.py
@@ -0,0 +1,5 @@
+TRAIN_TXT_PATH = "/content/train_record_class.txt"
+TEST_TXT_PATH = "/content/test_record_class.txt"
+TIME_COL = "Time (sec)"
+
+MODELS_PATH = "/content/drive/MyDrive/ML/training/Driver Behavior Classifier"
