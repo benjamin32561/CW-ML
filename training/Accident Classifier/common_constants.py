7053b8c5bd2203e507910e6b8504c36febdc94b7
benjamin32561 <ben32561@gmail.com>
2023-01-03 11:43:59 +0200

    added variable

--- training/Accident Classifier/common_constants.py
+++ training/Accident Classifier/common_constants.py
@@ -1,5 +1,5 @@
+ALL_TXT_PATH = "/content/record_class.txt"
 TRAIN_TXT_PATH = "/content/train_record_class.txt"
 TEST_TXT_PATH = "/content/test_record_class.txt"
-TIME_COL = "Time (sec)"
 
 MODELS_PATH = "/content/drive/MyDrive/ML/training/Accident Classifier"
