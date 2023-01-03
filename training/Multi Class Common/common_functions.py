18b41bd0e59eb655d8f75499161e1b1793d99659
benjamin32561 <ben32561@gmail.com>
2023-01-03 12:36:05 +0200

    now using same dataset type for all multi class

--- training/Multi Class Common/common_functions.py
+++ training/Multi Class Common/common_functions.py
@@ -7,7 +7,7 @@ from torch.utils.data import Dataset
 from torch.nn.functional import one_hot
 from data_analysis.functions import GetDataFrame, PlotRecordData, CreatePath
 
-class AccidentLocationClassifierAcceleratorDataset(Dataset):
+class MultiClassClassifierAcceleratorDataset(Dataset):
     def __init__(self, file_list, num_class):
         f = open(file_list)
         self.files = f.read().split('\n')
