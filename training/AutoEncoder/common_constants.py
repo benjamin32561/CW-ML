efd3df3eee4eedd9cf8f07e9d456683068b3ac3d
benjamin32561 <ben32561@gmail.com>
2023-01-03 12:50:21 +0200

    f

--- training/AutoEncoder/common_constants.py
+++ training/AutoEncoder/common_constants.py
@@ -1,1 +1,5 @@
+ALL_TXT_PATH = "/content/record_class.txt"
+TRAIN_TXT_PATH = "/content/train_record_class.txt"
+TEST_TXT_PATH = "/content/test_record_class.txt"
+
 MODELS_PATH = "/content/drive/MyDrive/ML/training/AutoEncoder"
