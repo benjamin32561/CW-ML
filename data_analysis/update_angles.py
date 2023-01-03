20f520be0a29e92f76ad03060496e9e5bfb9b0a0
benjamin32561 <ben32561@gmail.com>
2023-01-03 14:10:46 +0200

    now can specify angles

---
+++ data_analysis/update_angles.py
@@ -0,0 +1,51 @@
+import argparse
+import glob
+from os import rename
+from os.path import join, basename, exists
+from functions import GetDataFrame, CalcFrequency, CreatePath
+
+#python load_angles.py --angles_txt_path "" --time_to_angle_txt_path ""
+
+def main(args=None):
+    parser = argparse.ArgumentParser(description='Simple script for changing recording accident angle.')
+
+    parser.add_argument('--angles_txt_path', help='Path to .txt file containing .xlsx files path and their angles')
+    parser.add_argument('--time_to_angle_txt_path', help='Path to .txt file containing units, time and new angle')
+    parser = parser.parse_args(args)
+
+    assert parser.angles_txt_path!=None, "angles_txt_path can't be None"
+    assert exists(parser.angles_txt_path), "angles_txt_path does not exist"
+    assert parser.time_to_angle_txt_path!=None, "time_to_angle_txt_path can't be None"
+    assert exists(parser.time_to_angle_txt_path), "time_to_angle_txt_path does not exist"
+        
+    f = open(parser.angles_txt_path)
+    recordings_and_angles = f.read().split('\n')
+    f.close()
+    n_recordings = len(recordings_and_angles)
+
+    f = open(parser.time_to_angle_txt_path)
+    lines = f.read().split('\n')
+    f.close()
+    angle_by_time = {}
+    for line in lines:
+        units,time,angle = line.split(' ')
+        units = units.split(',')
+        angle_by_time[time]={"angle":angle,"units":units}
+    
+    for i in range(n_recordings):
+        recording = recordings_and_angles[i]
+        path, recording_angle = recording.split('\t')
+        for key in self.angle_by_time.keys():
+            if key in path:
+                for unit in self.angle_by_time[key]['units']:
+                    if unit in path:
+                        recording_angle = self.angle_by_time[key]['angle']
+        recordings_and_angles[i] = '\t'.join([path, recording_angle])
+
+
+    f = open(parser.angles_txt_path,'w')
+    f.write('\n'.join(recordings_and_angles))
+    f.close()
+        
+if "__main__"==__name__:
+    main()
--- training/Accident Angle Regressor/common_functions.py
+++ training/Accident Angle Regressor/common_functions.py
@@ -8,19 +8,10 @@ from torch.nn.functional import one_hot
 from data_analysis.functions import GetDataFrame, PlotRecordData, CreatePath
 
 class RegressionDataset(Dataset):
-    def __init__(self, file_list,angle_by_time):
+    def __init__(self, file_list):
         f = open(file_list)
         self.files = f.read().split('\n')
         f.close()
-
-        f = open(angle_by_time)
-        lines = f.read().split('\n')
-        f.close()
-        self.angle_by_time = {}
-        for line in lines:
-            units,time,angle = line.split(' ')
-            units = units.split(',')
-            self.angle_by_time[time]={"angle":angle,"units":units}
 
         self.n_files = len(self.files)
 
@@ -29,11 +20,6 @@ class RegressionDataset(Dataset):
 
     def GetDFAndClass(self, file_path:str, as_df=False):
         path, record_angle = file_path.split('\t')
-        for key in self.angle_by_time.keys():
-            if key in path:
-                for unit in self.angle_by_time[key]['units']:
-                    if unit in path:
-                        record_angle = self.angle_by_time[key]['angle']
         angle_to_ret = np.array([float(record_angle)/360])
         df = GetDataFrame(path)
         if not as_df:
